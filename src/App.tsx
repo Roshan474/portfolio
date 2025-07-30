cbb4f7c1f4b7bd9a9fe2b580ce70175d5eca82b1
Roshan474 <roshu176@gmail.com>
2025-07-30 12:19:17 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -273,7 +273,7 @@ function App() {
       </div>
     <div className="flex flex-col items-center mt-6">
   <a
-    href="/Roshan_Resume.pdf"
+    href="/Roshan(Resume).pdf"
     download="Roshan_Resume.pdf"
     className="px-6 py-3 flex items-center justify-center rounded-full border-2 border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white relative shadow-xl hover:scale-105 transition-all duration-500 group"
     style={{
