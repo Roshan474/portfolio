6d495dc187a46c35cc521064fcd5b98989d99943
Roshan474 <roshu176@gmail.com>
2026-03-04 12:15:06 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -285,7 +285,7 @@ function App() {
       </div>
     <div className="flex flex-col items-center mt-6">
   <a
-    href="/Roshan(Resume).pdf"
+    href="/ROSHAN__RESUME.pdf"
     download="ROSHAN__RESUME.pdf"
     className="px-6 py-3 flex items-center justify-center rounded-full border-2 border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white relative shadow-xl hover:scale-105 transition-all duration-500 group"
     style={{
