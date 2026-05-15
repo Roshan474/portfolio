e13fceddb80dfe8582bc3401b7e69295fe62ba3a
Roshan474 <roshu176@gmail.com>
2026-05-15 12:57:26 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -285,8 +285,8 @@ function App() {
       </div>
     <div className="flex flex-col items-center mt-6">
   <a
-    href="/ROSHAN__RESUME.pdf"
-    download="ROSHAN__RESUME.pdf"
+    href="/RoshanResume4.pdf"
+    download="RoshanResume.pdf"
     className="px-6 py-3 flex items-center justify-center rounded-full border-2 border-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-white relative shadow-xl hover:scale-105 transition-all duration-500 group"
     style={{
       backgroundClip: 'padding-box',
